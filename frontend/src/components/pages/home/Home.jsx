e7aa819f2734b1f25f4aa60797658ffea5515c20
nayimHossen <nayimh2015@gmail.com>
2022-09-13 18:45:26 +0600

    articals component

--- frontend/src/components/pages/home/Home.jsx
+++ frontend/src/components/pages/home/Home.jsx
@@ -3,38 +3,13 @@ import PopularArtical from "./PopularArtical";
 import SocilePage from "./SocilePage";
 import Categorys from "./Categorys";
 import Tags from "./Tags";
+import HomeArtical from "./HomeArtical";
 
 function Home() {
   return (
     <div className="flex  max-w-7xl mx-auto px-4 md:px-12 py-5">
       <div class="w-[70%] mr-4">
-        <div class="grid grid-cols-2 gap-4">
-          <div className="card card-side bg-base-100 shadow-xl mb-4">
-            <figure>
-              <img src="https://placeimg.com/200/280/arch" alt="Movie" />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">New movie is released!</h2>
-              <p>Click the button to watch on Jetflix app.</p>
-              <div className="card-actions justify-end">
-                <button className="btn btn-primary">Watch</button>
-              </div>
-            </div>
-          </div>
-
-          <div className="card card-side bg-base-100 shadow-xl mb-4">
-            <figure>
-              <img src="https://placeimg.com/200/280/arch" alt="Movie" />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">New movie is released!</h2>
-              <p>Click the button to watch on Jetflix app.</p>
-              <div className="card-actions justify-end">
-                <button className="btn btn-primary">Watch</button>
-              </div>
-            </div>
-          </div>
-        </div>
+        <HomeArtical />
       </div>
 
       <div class="w-[30%]">
---
+++ frontend/src/components/pages/home/HomeArtical.jsx
@@ -0,0 +1,97 @@
+function HomeArtical() {
+  return (
+    <div class="grid md:grid-cols-1 lg:grid-cols-2 gap-4">
+      <div>
+        <div className="card card-side bg-base-100 shadow-xl">
+          <figure>
+            <img src="https://placeimg.com/200/280/arch" alt="Movie" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">New movie is released!</h2>
+            <p>Click the button to watch on Jetflix app.</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Watch</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div className="card card-side bg-base-100 shadow-xl">
+          <figure>
+            <img src="https://placeimg.com/200/280/arch" alt="Movie" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">New movie is released!</h2>
+            <p>Click the button to watch on Jetflix app.</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Watch</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div className="card card-side bg-base-100 shadow-xl">
+          <figure>
+            <img src="https://placeimg.com/200/280/arch" alt="Movie" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">New movie is released!</h2>
+            <p>Click the button to watch on Jetflix app.</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Watch</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div className="card card-side bg-base-100 shadow-xl">
+          <figure>
+            <img src="https://placeimg.com/200/280/arch" alt="Movie" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">New movie is released!</h2>
+            <p>Click the button to watch on Jetflix app.</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Watch</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div className="card card-side bg-base-100 shadow-xl">
+          <figure>
+            <img src="https://placeimg.com/200/280/arch" alt="Movie" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">New movie is released!</h2>
+            <p>Click the button to watch on Jetflix app.</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Watch</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div className="card card-side bg-base-100 shadow-xl">
+          <figure>
+            <img src="https://placeimg.com/200/280/arch" alt="Movie" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">New movie is released!</h2>
+            <p>Click the button to watch on Jetflix app.</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Watch</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HomeArtical;
